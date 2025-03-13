a0da9770d72319052a301efc9c723ded34e23602
matheus <eurotheus@gmail.com>
2025-03-13 07:26:39 -0300

    chore: class correction and sending to remote repository

--- backend/src/main/java/flowmoney/backend/configuration/SecurityFilter.java
+++ backend/src/main/java/flowmoney/backend/configuration/SecurityFilter.java
@@ -8,10 +8,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
